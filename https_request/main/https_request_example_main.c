d989299c5de509b21dcb5ae2ba93ab217bdb978a
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-06-03 00:00:09 -0700

    get request complete

--- https_request/main/https_request_example_main.c
+++ https_request/main/https_request_example_main.c
@@ -102,6 +102,94 @@ static esp_tls_client_session_t *tls_client_session = NULL;
 static bool save_client_session = false;
 #endif
 
+#define PHONE_SERVER "10.0.0.179"
+#define PHONE_PORT "8000"
+#define PHONE_URL "http://10.0.0.179:8000/location"
+#define PHONE_PATH "/location"
+
+static const char *REQUEST = "GET " PHONE_PATH " HTTP/1.1\r\n"
+                             "Host: " PHONE_SERVER ":" PHONE_PORT "\r\n"
+                             "User-Agent: esp-idf/1.0 esp32\r\n"
+                             "\r\n";
+
+static void http_get_task(void) {
+  const struct addrinfo hints = {
+      .ai_family = AF_INET,
+      .ai_socktype = SOCK_STREAM,
+  };
+  struct addrinfo *res;
+  struct in_addr *addr;
+  int s, r;
+  char recv_buf[64];
+
+  int err = getaddrinfo(PHONE_SERVER, PHONE_PORT, &hints, &res);
+
+  if (err != 0 || res == NULL) {
+    ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    return;
+  }
+
+  /* Code to print the resolved IP.
+
+     Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code
+   */
+  addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+  ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+  s = socket(res->ai_family, res->ai_socktype, 0);
+  if (s < 0) {
+    ESP_LOGE(TAG, "... Failed to allocate socket.");
+    freeaddrinfo(res);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    return;
+  }
+  ESP_LOGI(TAG, "... allocated socket");
+
+  if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+    ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+    close(s);
+    freeaddrinfo(res);
+    vTaskDelay(4000 / portTICK_PERIOD_MS);
+    return;
+  }
+
+  ESP_LOGI(TAG, "... connected");
+  freeaddrinfo(res);
+
+  if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+    ESP_LOGE(TAG, "... socket send failed");
+    close(s);
+    vTaskDelay(4000 / portTICK_PERIOD_MS);
+    return;
+  }
+  ESP_LOGI(TAG, "... socket send success");
+
+  struct timeval receiving_timeout;
+  receiving_timeout.tv_sec = 5;
+  receiving_timeout.tv_usec = 0;
+  if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                 sizeof(receiving_timeout)) < 0) {
+    ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+    close(s);
+    vTaskDelay(4000 / portTICK_PERIOD_MS);
+    return;
+  }
+  ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+  /* Read HTTP response */
+  do {
+    bzero(recv_buf, sizeof(recv_buf));
+    r = read(s, recv_buf, sizeof(recv_buf) - 1);
+    for (int i = 0; i < r; i++) {
+      putchar(recv_buf[i]);
+    }
+  } while (r > 0);
+
+  ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.",
+           r, errno);
+  close(s);
+}
 static void https_get_request(esp_tls_cfg_t cfg, const char *WEB_SERVER_URL,
                               const char *REQUEST) {
   char buf[512];
@@ -168,6 +256,7 @@ static void https_get_request(esp_tls_cfg_t cfg, const char *WEB_SERVER_URL,
       break;
     } else if (ret == 0) {
       ESP_LOGI(TAG, "connection closed");
+      http_get_task();
       break;
     }
 
