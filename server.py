ac45e88fc45f679e86ded7e84e406ce1d3cc6e8e
Ankur Ahir <ahir.ankur0429@gmail.com>
2024-06-02 20:02:48 -0700

    changed server

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.request
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b"Hello, World!")
-        
+        if self.path == '/location':
+            self.handle_location_request()
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not Found")
+
+    def handle_location_request(self):
+        with urllib.request.urlopen('http://wttr.in?format=%l\n') as response:
+            data = response.read().decode('utf-8').strip()
+            location = data.split(',')[0].replace(' ', '+')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(location.encode('utf-8'))
+
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
